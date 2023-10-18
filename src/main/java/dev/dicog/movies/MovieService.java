caef72e03f032ba8139d91f31b3943f3dfa678af
Diego Gonzalez <diegom.develops@hotmail.com>
2023-10-18 11:45:32 -0700

    feat: Review Document
    Review Document, Controller, Service and Repository

--- src/main/java/dev/dicog/movies/MovieService.java
+++ src/main/java/dev/dicog/movies/MovieService.java
@@ -1,9 +1,11 @@
 package dev.dicog.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -12,4 +14,9 @@ public class MovieService {
     public List<Movie> allMovies() {
         return movieRepository.findAll();
     }
+
+    public Optional<Movie> singleMovie(String id) {
+        return movieRepository.findMovieByImdbId(id);
+    }
+
 }
